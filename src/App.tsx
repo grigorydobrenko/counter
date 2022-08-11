42d5bc8936059c93a5dbfe7a32a6401bd63148cd
grigorydobrenko <grigorydobrenko97@gmail.com>
2022-08-11 12:24:40 +0300

    add Button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from "./Components/Counter";
+import s from "./Components/Counter.module.css";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const startValue = 0;
+    const endValue = 5;
+
+    const [value, setValue] = useState<number>(startValue)
+
+    const [incDisabled, setIncDisabled] = useState<boolean>(false)
+    const [resetDisabled, setResetDisabled] = useState<boolean>(true)
+
+    const incSetValue = () => setValue(value => value + 1)
+
+    const incValue = () => {
+        if (value < endValue && value !== endValue - 1) {
+            setResetDisabled(false)
+        } else {
+            setIncDisabled(true)
+        }
+        incSetValue()
+    }
+
+    const resetValue = () => {
+        if (value !== startValue) {
+            setValue(startValue)
+            setResetDisabled(true)
+            incDisabled && setIncDisabled(false)
+        }
+
+    }
+
+
+    return (
+        <div className="App">
+
+            <Counter
+                incValue={incValue}
+                resetValue={resetValue}
+                value={value}
+                incDisabled={incDisabled}
+                resetDisabled={resetDisabled}
+            />
+        </div>
+    );
+}
+
+
+const CounterSetValues = () => {
+    return (
+        <div className={s.container}>
+            <div>
+                <div>max value: <input type="number"/></div>
+                <div>min value: <input type="number"/></div>
+            </div>
+            <div className={s.buttons}>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
